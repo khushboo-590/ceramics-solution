3cad248cdcba1611c4c8b66db2e118c37996fe82
khushboo-590 <khushbook4979@gmail.com>
2025-02-04 20:37:44 +0530

    hero-section with navbar

---
+++ assets/js/app .js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $('.my-file').slick({
+        dots: true,
+        infinite: true,
+        // speed: 50,
+        slidesToShow: 3,
+        // autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+          
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+        ],
+
+    });
+});
+
+
+let menuIcon = document.querySelector(".menu-icon");
+let menuList = document.querySelector(".menu-list");
+menuIcon.addEventListener("click", () => {
+    menuList.classList.toggle("show")
+    let activeNav = menuList.hasAttribute("class", "show");
+    if (activeNav) { menuIcon.classList.toggle("active") };
+})
